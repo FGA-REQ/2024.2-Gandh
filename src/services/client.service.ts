319645d58e78b18504495b1af9ebb670473709e5
andre-maia51 <acmc.0410@gmail.com>
2025-01-24 22:07:27 -0300

    refactor: adicionando mais verificações na autenticação do cliente

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { ClientRepository } from '../repositories/client.repository';
 import * as bcrypt from 'bcrypt';
 
@@ -16,6 +16,14 @@ export class ClientService {
 
   async checkLogIn(credentials: {gmail: string, password: string}) {
     const client = await this.clientRepository.findOneByEmail(credentials.gmail);
+
+    if(credentials.gmail == null) {
+      throw new BadRequestException('O campo do email deve ser preenchido!');
+    }
+
+    if(credentials.password == null) {
+      throw new BadRequestException('O campo da senha deve ser preenchido!');
+    }
 
     if(!client) {
       throw new UnauthorizedException('Usuário não encontrado! Verifique se o email está correto.');
