3585d93a084bf81b992c4328a301078b2e794758
Natália Rodrigues <natytotherodrigues@gmail.com>
2025-02-01 02:03:22 -0300

    add: API's to upload and show url for photos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { AdminModule } from './modules/admin.module';
 import { ClientModule } from './modules/client.module';
 import { AuthModule } from './modules/auth.module';
 import { MenuModule } from './modules/menu.module';
+import { UploadModule } from './modules/upload.module';
+
 
 @Module({
-  imports: [AdminModule, ClientModule, AuthModule, MenuModule],
+  imports: [AdminModule, ClientModule, AuthModule, MenuModule, UploadModule],
 })
 export class AppModule {}
