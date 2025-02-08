11809a231860cb95ae090942c4c45db435bea2cc
Natália Rodrigues <natytotherodrigues@gmail.com>
2025-02-08 18:14:43 -0300

    add: time validation on requests

--- src/services/request.service.ts
+++ src/services/request.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable,BadRequestException } from '@nestjs/common';
 import { RequestRepository } from '../repositories/request.repository';
 
 @Injectable()
@@ -6,6 +6,10 @@ export class RequestService {
   constructor(private readonly requestRepository: RequestRepository) {}
 
   async createOrder(cartId: number, address: string | undefined, deliveryOption: number, orderDetails: string | undefined) {
+    if (!this.isWithinOperatingHours()) {
+      throw new BadRequestException('Pedidos só podem ser realizados entre 18:00 e 23:00.');
+    }
+    
     return await this.requestRepository.createOrder(cartId, address, deliveryOption, orderDetails);
   }
 
@@ -20,4 +24,11 @@ export class RequestService {
   async getOrderDetails(requestId: number) {
     return await this.requestRepository.getOrderDetails(requestId);
   }  
+
+  private isWithinOperatingHours(): boolean {
+    const now = new Date();
+    const currentHour = now.getHours();
+    
+    return currentHour >= 18 && currentHour < 23;
+  }
 }
