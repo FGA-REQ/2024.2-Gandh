f0095fb79a7a6b036815ed4f3ac8214b38889ca4
DanielFsR <danielferreirasantosd12@gmail.com>
2025-02-09 19:07:34 -0300

    add test coverage

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,17 @@
 module.exports = {
-    moduleNameMapper: {
-      '^src/(.*)$': '<rootDir>/src/$1',
-    },
-    transform: {
-      '^.+\\.ts$': 'ts-jest',
-    },
-    testEnvironment: 'node',
-  };
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testEnvironment: 'node',
+  collectCoverage: true, // Ativa a cobertura
+  collectCoverageFrom: [
+    'src/**/*.ts', // Inclui todos os arquivos TypeScript dentro de src
+    '!src/main.ts', // Exemplo: exclui main.ts da cobertura
+    '!src/**/index.ts', // Exclui arquivos index.ts
+  ],
+  coverageDirectory: 'coverage', // Define a pasta de saída
+  coverageReporters: ['html', 'text', 'lcov'], // Define os formatos de relatório
+};
